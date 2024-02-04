2ea89a2f362a6af162bb49c326f6a4b86293c020
2bato <andrewfan030114@gmail.com>
2024-02-04 15:30:27 -0800

    feat: add basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 "use client";
-import NearbySearch from "@/components/nearby-search";
+import Filters from "@/components/filter";
+import NearbySearch from "@/utils/nearby-search";
 import { useState, useEffect } from "react";
+
+interface Result {
+  displayName: string;
+  photoUrl: string;
+  rating: number;
+}
 
 export default function Home() {
   const [location, setLocation] = useState<{
@@ -9,9 +16,30 @@ export default function Home() {
   } | null>(null);
 
   const [fetchLocation, setFetchLocation] = useState<boolean>(true);
+  const [displayName, setDisplayName] = useState<string>("");
+  const [displayRating, setDisplayRating] = useState<number>(0);
+  const [displayPhoto, setDisplayPhoto] = useState<string>("");
+  const [rating, setRating] = useState<number>(0);
+  const [price, setPrice] = useState<string>("");
+  const [cuisine, setCuisine] = useState<string>("restaurant");
 
-  const handleButtonClick = () => {
+  const handleButtonClick = async () => {
     setFetchLocation(true);
+    const result: Result = await NearbySearch(cuisine, rating, price);
+    setDisplayName(result.displayName);
+    setDisplayRating(result.rating);
+    setDisplayPhoto(result.photoUrl);
+    console.log(displayName);
+  };
+
+  const handleFilterChange = (
+    newRating: number,
+    newPrice: string,
+    newCuisine: string
+  ) => {
+    setRating(newRating);
+    setPrice(newPrice);
+    setCuisine(newCuisine);
   };
 
   useEffect(() => {
@@ -27,15 +55,18 @@ export default function Home() {
     }
   }, [fetchLocation]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div></div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
+      <div>
+        {" "}
+        {displayName} {displayRating}{" "}
+        <img src={displayPhoto} alt="Image Description" />
+      </div>
       <button
         onClick={handleButtonClick}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       ></button>
-      {location?.latitude},
-      {location?.longitude}
-      <NearbySearch />
+      {location?.latitude},{location?.longitude}
+      <Filters onFilterChange={handleFilterChange} />
     </main>
   );
 }
