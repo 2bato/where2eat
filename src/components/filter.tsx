6c368f904c6a144def55c8133364ea2c4f8ce23a
2bato <andrewfan030114@gmail.com>
2024-02-04 23:15:02 -0800

    feat: add distance filter and improve ui

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,28 +1,68 @@
-import { useState } from "react";
+import { ChangeEvent, useState, useEffect } from "react";
 
 interface filterProps {
-  onFilterChange: (rating: number, price: string, cuisine: string) => void;
+  onFilterChange: (
+    rating: number[],
+    price: string[],
+    cuisine: string[],
+    radius: number
+  ) => void;
 }
 
 export default function Filters({ onFilterChange }: filterProps) {
-  const [rating, setRating] = useState<number>(0);
-  const [price, setPrice] = useState<string>("");
-  const [cuisine, setCuisine] = useState<string>("");
+  const [ratings, setRatings] = useState<number[]>([]);
+  const [prices, setPrices] = useState<string[]>([]);
+  const [cuisines, setCuisines] = useState<string[]>([]);
+  const [dist, setDist] = useState<number>(1000);
+
+  useEffect(() => {
+    onFilterChange(ratings, prices, cuisines, dist);
+  }, [ratings, prices, cuisines, dist, onFilterChange]);
 
   const handleRatingChange = (newRating: number) => {
-    setRating(newRating);
-    onFilterChange(newRating, price, cuisine);
+    setRatings((prevRatings) => {
+      const updatedRatings = prevRatings.includes(newRating)
+        ? prevRatings.filter((rating) => rating !== newRating)
+        : [...prevRatings, newRating];
+
+      return updatedRatings;
+    }),
+      () => {
+        onFilterChange(ratings, prices, cuisines, dist);
+      };
   };
 
   const handlePriceChange = (newPrice: string) => {
-    setPrice(newPrice);
-    onFilterChange(rating, newPrice, cuisine);
+    setPrices((prevPrices) => {
+      const updatedPrices = prevPrices.includes(newPrice)
+        ? prevPrices.filter((price) => price !== newPrice)
+        : [...prevPrices, newPrice];
+      return updatedPrices;
+    }),
+      () => {
+        onFilterChange(ratings, prices, cuisines, dist);
+      };
   };
 
   const handleCuisineChange = (newCuisine: string) => {
-    setCuisine(newCuisine);
-    onFilterChange(rating, price, newCuisine);
+    setCuisines((prevCuisines) => {
+      const updatedCuisines = prevCuisines.includes(newCuisine)
+        ? prevCuisines.filter((cuisine) => cuisine !== newCuisine)
+        : [...prevCuisines, newCuisine];
+
+      return updatedCuisines;
+    }),
+      () => {
+        onFilterChange(ratings, prices, cuisines, dist);
+      };
   };
+
+  const handleDistChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const updatedDist = parseInt(event.target.value);
+    setDist(updatedDist);
+    onFilterChange(ratings, prices, cuisines, updatedDist);
+  };
+
   const ratingOptions = [3, 4, 5];
   const priceOptions = [
     "PRICE_LEVEL_INEXPENSIVE",
@@ -37,47 +77,52 @@ export default function Filters({ onFilterChange }: filterProps) {
     "mexican_restaurant",
     "japanese_restaurant",
     "indian_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
-    "sushi_restaurant",
+    "greek_restaurant",
+    "french_restaurant",
+    "brazilian_restaurant",
+    "vegetarian_restaurant",
+    "mediterranean_restaurant",
+    "vietnamese_restaurant",
     "sushi_restaurant",
   ];
   return (
     <div className="flex flex-col items-center mb-8 space-y-2">
-      <div className="space-y-2">
-        <div className="flex flex-row space-x-2">
+      <div className="space-y-2 items-center flex flex-col">
+        <div className="flex flex-row space-x-5">
           {ratingOptions.map((option) => (
             <button
               key={option}
-              className={`text-black text-sm rounded-2xl px-5 py-2 ${
-                rating === option ? "bg-blue-500" : "bg-gray-50"
+              className={`text-black text-sm rounded-2xl px-4 py-2 ${
+                ratings.includes(option) ? "bg-blue-500" : "bg-gray-50"
               }`}
-              onClick={() => handleRatingChange(option)}
+              onClick={() => {
+                handleRatingChange(option);
+              }}
             >
               {option === 3 && "3 ★"}
               {option === 4 && "4 ★"}
               {option === 5 && "5 ★"}
             </button>
           ))}
+          <select
+            onChange={handleDistChange}
+            className="focus:outline-none bg-gray-50 text-black text-sm rounded-2xl pl-2 py-2"
+          >
+            <option value="1000">1 km</option>
+            <option value="500">500 m</option>
+            <option value="2000">2 km</option>
+          </select>
         </div>
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row space-x-4">
           {priceOptions.map((option) => (
             <button
               key={option}
-              className={`text-gray-900 text-sm rounded-2xl px-3 py-2 ${
-                price === option ? "bg-blue-500" : "bg-gray-50 "
+              className={`text-black text-sm rounded-2xl px-4 py-2 ${
+                prices.includes(option) ? "bg-blue-500" : "bg-gray-50 "
               }`}
-              onClick={() => handlePriceChange(option)}
+              onClick={() => {
+                handlePriceChange(option);
+              }}
             >
               {option === "PRICE_LEVEL_INEXPENSIVE" && "$"}
               {option === "PRICE_LEVEL_MODERATE" && "$$"}
@@ -87,14 +132,16 @@ export default function Filters({ onFilterChange }: filterProps) {
           ))}
         </div>
       </div>
-      <div className="flex flex-row overflow-x-scroll max-w-80">
+      <div className="flex flex-row overflow-x-scroll space-x-2 max-w-80">
         {cuisineOptions.map((option) => (
           <button
             key={option}
-            className={`border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 focus:ring-blue-500 focus:border-blue-500 ${
-              cuisine === option ? "bg-blue-500" : "bg-gray-50 "
+            className={`text-black text-sm rounded-2xl p-2.5 ${
+              cuisines.includes(option) ? "bg-blue-500" : "bg-gray-50 "
             }`}
-            onClick={() => handleCuisineChange(option)}
+            onClick={() => {
+              handleCuisineChange(option);
+            }}
           >
             {option.replace("_restaurant", "")}
           </button>
